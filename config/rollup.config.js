8babc48bcef0d30052eb48a6975a0eb7356d92f0
Arjun Raju Pillai <revarjunr@gmail.com>
2020-04-15 18:49:27 +0530

    Project initial version complete

---
+++ config/rollup.config.js
@@ -0,0 +1,36 @@
+// const babel = require('rollup-plugin-babel');
+const { terser } = require('rollup-plugin-terser');
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const typescript = require('@rollup/plugin-typescript');
+// const pkg = require('./package.json');
+
+module.exports = {
+  input: 'src/index.ts',
+  // external: [makeExternalPredicate(externals)],
+  output: [
+    // {
+    //   file: 'bundle.js',
+    //   dir: 'dist',
+    //   format: 'cjs',
+    // },
+    {
+      file: 'libs/bundle.min.js',
+      format: 'umd',
+      name: 'version',
+      plugins: [terser()],
+    },
+  ],
+  plugins: [
+    resolve({
+      customResolveOptions: {
+        moduleDirectory: 'node_modules',
+      },
+    }),
+    commonjs(),
+    typescript(),
+    // babel({
+    //   exclude: 'node_modules/**',
+    // }),
+  ],
+};
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+function findHighestZIndex() {
+  const elems = document.getElementsByTagName("*");
+  let highest = 0;
+  for (let i = 0; i < elems.length; i++) {
+    const zindex = parseInt(document.defaultView.getComputedStyle(elems[i], null).getPropertyValue("z-index"));
+    if ((zindex > highest)) {
+      highest = zindex;
+    }
+  }
+  return highest;
+}
+
+export default findHighestZIndex;
